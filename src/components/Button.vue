10108b828f0862e70de8fcc6b4f74d835daa6164
Kaique Nascente <kaiquenascente@gmail.com>
2021-03-14 20:39:24 -0300

    First Version Dependence

---
+++ src/components/Button.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="button"
+    @click="$emit('onClick', label)"
+    :class="{double, triple, operation}" >
+
+     {{label}}
+     
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        label: {},
+        operation: {type: Boolean},
+        double: {type: Boolean},
+        triple: {type: Boolean}
+        
+    }
+}
+</script>
+
+<style>
+    :root {
+        --bg-button: #f0f0f0;
+        --border-button: solid 1px #888;
+    }
+
+    .button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
+        font-size: 1.4em;
+        background-color: var(--bg-button);
+        border: none;
+        border-right: var(--border-button);
+        border-bottom: var(--border-button);
+        outline: none;
+    }
+
+    .button:active {
+        background-color: #ccc;
+    }
+
+    .button.double {
+        grid-column: span 2;
+    }
+
+    .button.triple {
+        grid-column: span 3;
+    }
+
+    .button.operation {
+        background-color: aqua;
+    }
+</style>
